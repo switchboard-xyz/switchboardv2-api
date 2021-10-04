b367e5ffb1cca6559565379cfb5e2428ecb85d8e
mgild <mgild@users.noreply.github.com>
2021-10-04 09:17:59 -0400

    debugging

--- dist/sbv2.js
+++ dist/sbv2.js
@@ -935,13 +935,13 @@ class CrankAccount {
      * @return newly generated CrankAccount.
      */
     static async create(program, params) {
-        var _a, _b;
+        var _a;
         const crankAccount = anchor.web3.Keypair.generate();
         const size = program.account.crankAccountData.size;
         await program.rpc.crankInit({
             id: ((_a = params.id) !== null && _a !== void 0 ? _a : Buffer.from("")).slice(0, 32),
-            unused: 5,
-            metadata: ((_b = params.metadata) !== null && _b !== void 0 ? _b : Buffer.from("")).slice(0, 64),
+            // unused: 5, // resolves borsh bug in deserialization.
+            // metadata: (params.metadata ?? Buffer.from("")).slice(0, 64),
         }, {
             accounts: {
                 crank: crankAccount.publicKey,
