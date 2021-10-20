1475354ad2689ccec1f6415ae5e596d7ca7cc7ef
mgild <mgild@users.noreply.github.com>
2021-10-20 12:51:32 -0400

    assert big conversions

--- src/sbv2.ts
+++ src/sbv2.ts
@@ -55,7 +55,7 @@ export class SwitchboardDecimal {
     mantissa = mantissa.mul(new anchor.BN(big.s, 10));
 
     const result = new SwitchboardDecimal(mantissa, scale);
-    assert.ok(big === result.toBig());
+    assert.ok(big.eq(result.toBig()));
     return result;
   }
 
