4d5953a2d1b0cc800c6f489d6074b9b5b8028e1d
mgild <mgild@users.noreply.github.com>
2022-01-04 10:53:57 -0500

    big cleanup

--- src/sbv2.ts
+++ src/sbv2.ts
@@ -85,32 +85,32 @@ export class SwitchboardDecimal {
    * @return Big representation
    */
   public toBig(): Big {
-    // let mantissa = new anchor.BN(this.mantissa, 10);
-    // let s = 1;
-    // let c: Array<number> = [];
-    // const ZERO = new anchor.BN(0, 10);
-    // const TEN = new anchor.BN(10, 10);
-    // if (mantissa.lt(ZERO)) {
-    // s = -1;
-    // mantissa = mantissa.abs();
-    // }
-    // while (mantissa.gt(ZERO)) {
-    // c.unshift(mantissa.mod(TEN).toNumber());
-    // mantissa = mantissa.div(TEN);
-    // }
-    // let e = this.scale - c.slice(1).length;
-    // let result = new Big(0);
-    // result.s = s;
-    // result.c = c;
-    // result.e = e;
-    // return result;
+    let mantissa = new anchor.BN(this.mantissa, 10);
+    let s = 1;
+    let c: Array<number> = [];
+    const ZERO = new anchor.BN(0, 10);
+    const TEN = new anchor.BN(10, 10);
+    if (mantissa.lt(ZERO)) {
+      s = -1;
+      mantissa = mantissa.abs();
+    }
+    while (mantissa.gt(ZERO)) {
+      c.unshift(mantissa.mod(TEN).toNumber());
+      mantissa = mantissa.div(TEN);
+    }
+    let e = c.length - this.scale - 1;
+    let result = new Big(0);
+    result.s = s;
+    result.c = c;
+    result.e = e;
+    return result;
 
-    const decimalIndex = this.mantissa.toString().length - this.scale;
-    return new Big(
-      `${this.mantissa.toString().slice(0, decimalIndex)}.${this.mantissa
-        .toString()
-        .slice(decimalIndex)}`
-    );
+    // const decimalIndex = this.mantissa.toString().length - this.scale;
+    // return new Big(
+    // `${this.mantissa.toString().slice(0, decimalIndex)}.${this.mantissa
+    // .toString()
+    // .slice(decimalIndex)}`
+    // );
   }
 }
 
